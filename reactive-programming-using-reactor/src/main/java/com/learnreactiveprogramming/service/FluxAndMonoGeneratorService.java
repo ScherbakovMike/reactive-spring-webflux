d469474e01ae266f744f76fc0af03f916abbb24b
ScherbakovMike <scherbakov.mike@gmail.com>
2023-09-23 19:06:44 +0300

    Init commit

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,18 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"));
+    }
+
+    public static void main(String[] args) {
+        var service = new FluxAndMonoGeneratorService();
+        service.namesFlux()
+                .subscribe(name -> System.out.printf("Name is: %s%n", name));
+    }
 }
